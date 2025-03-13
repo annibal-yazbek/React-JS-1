6319d960d990efa2a56bc5dea5220edd71c57e6e
Annibal <annibal.yazbek@icloud.com>
2025-03-13 12:47:32 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import JsxExamples from "./components/jsxExamples";
+
+function App() {
+  return (
+    <div>
+      <h1>JSX</h1>
+      <JsxExamples />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/JsxExamples.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const JsxExamples = () => {
+  return (
+    <div>
+      <h2>Conteúdo que o usuário vai ver</h2>
+      {/* Listando dados do usuário */}
+    </div>
+  );
+};
+
+export default JsxExamples;
